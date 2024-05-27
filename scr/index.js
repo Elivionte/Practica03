01694a9f4197f865cacf662afdaa1d7e6acef21e
Cuchufleitor <luisestebanordenana@gmail.com>
2024-05-27 20:26:42 -0300

    esta vez tiene una foto

--- scr/index.js
+++ scr/index.js
@@ -1,1 +1,17 @@
-console.log('Todavia no hice nada')
+const express = require('express')
+
+const app = express()
+
+app.get('/fotito', (req, res) => {
+    res.sendFile('./AguanteBoca.jpg', {
+        root: __dirname
+    })
+})
+
+app.get('/personas', (req, res) => {
+    res.json({name: "roberto"})
+})
+
+app.listen(3000)
+
+console.log('el servidor escucha!')
